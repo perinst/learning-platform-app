5261f88946bee702b593cf582c4350b3f9679ee3
vinhpm <phanminhvinh3010023@gmail.com>
2025-11-05 17:29:02 +0700

    feat(api): cors for new domain

--- api/src/config/cors.config.ts
+++ api/src/config/cors.config.ts
@@ -4,6 +4,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
   "https://zlearning.vercel.app",
+  "https://ui.sgk.guidestaredu.com",
 ];
 
 export const corsOptions: CorsOptions = {
